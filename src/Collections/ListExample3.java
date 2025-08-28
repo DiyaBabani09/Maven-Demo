1ab2842f56d12e6c8cfeb5edd30c06ce36bf845c
DiyaBabani09 <diyababani03@gmail.com>
2025-08-28 16:05:55 +0530

    add new file of collections

---
+++ src/Collections/ListExample3.java
@@ -0,0 +1,18 @@
+package Collections;
+
+import java.util.LinkedList;
+
+public class ListExample3 {
+    public static void main(String[] args) {
+        LinkedList<Integer> l = new LinkedList<>();
+        l.add(01);
+        l.add(9);
+        l.add(03);
+        l.add(07);
+//        for (int i : l) {
+//            System.out.println(i);
+//        }
+boolean ans=l.contains(9);
+        System.out.println(true);
+    }
+}
